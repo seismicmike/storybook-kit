7f9d3f3e8f7febdd00eb2110aafa631e8e5f71b7
Michael Lewis <seismicmike@gmail.com>
2025-06-11 11:21:59 -0400

    Scaffold the Social Button Component

---
+++ docroot/themes/custom/sb_bazo/components/social_button/social_button.stories.jsx
@@ -0,0 +1,29 @@
+
+// AUTO-GENERATED FROM social_button.stories.yml
+import { createMockAttributes } from '@storybook-utils/mockAttributes';
+import Component from './social_button.twig';
+import './social_button.css';
+
+
+export default {
+    title: 'SB Bazo/Social Button',
+    argTypes: {},
+    component: Component
+};
+
+
+export const Maps = {
+    parameters: {
+      docs: {
+        description: {
+          story: '',
+        }
+      }
+    },
+    args: {
+  "icon": "location-dot",
+  "size": "sm",
+  "url": "https://maps.google.com"
+, 'base_path': 'docroot/themes/custom/sb_bazo', 'attributes': createMockAttributes().setAttribute('data-component-id', 'sb_bazo:social_button')}
+};
+
